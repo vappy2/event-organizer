fef47907c1d107ea8e0a7b515eff1ce36818f003
vappy2 <43212809+vappy2@users.noreply.github.com>
2022-06-30 16:44:55 +0200

    :lipstick: Some css for the UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import events from './data/events.json';
 import Event from './components/Event';
 
 function App() {
+
+  const listEvents = events.map((event) => {
+    return(
+      <Event key={event.id} 
+    title={event.title} 
+    label={event.label} 
+    date={event.date} 
+    emailCreator={event.emailCreator} 
+    description={event.description} />
+    )
+  })
+    
+
   return (
     <div className="App">
+      <h1>Liste des événements</h1>
       <div className='eventContainer'>
+
+        {listEvents}
 
       </div>
     </div>
